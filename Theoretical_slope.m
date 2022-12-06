43a332cad571cbe810137812851c0eb37bff7a1c
grg.dll <grg.dll@gmail.com>
2022-12-06 14:01:44 +0000

    tidied up code, added modifications so that it can work on Octave (in a reasonable amount of time)

--- Theoretical_slope.m
+++ Theoretical_slope.m
@@ -26,139 +26,185 @@ function [VSF,d_VSF, beam_c, d_beam_c,ratio,unc_ratio] = Theoretical_slope(D0,er
 % For any comments/questions please contact: emmanuel.boss@maine.edu
 %  Emmanuel Boss 12-05-2022 + help from Giorgio Dall'Olmo
 
-%%% used in all Mie computations
-accept_angle=0.93; % acceptance angle of transmissometer
-nang=100*3+1; %number of angles for mie computation
-dang=pi/2/(nang-1); %angular resolution in radians
-%n_ang=2*nang-1; %number of angles for computation
-ang=[0:dang:pi]*180/pi; %angles in degrees
-T=24; %temperature of calibration water, for index of refraction computations
+% check is Octave is being run, and if so, load needed packages
+	if exist('OCTAVE_VERSION',  'builtin')
+		pkg load statistics
+		pkg load nan
+	end
 
-NN=length(D0); %number of beads
-KK=length(wl); %number of wavelengths of beta 
-JJ=length(c_wl); %number of wavelengths of beam_c
-now_1=now;
+	%%% used in all Mie computations
+	accept_angle = 0.93; % acceptance angle of transmissometer
+	nang = 100*3 + 1; %number of angles for mie computation
+	dang = pi/2/(nang - 1); %angular resolution in radians
+	ang = [0: dang: pi]*180/pi; %angles in degrees
+	T = 24; %temperature of calibration water,  for index of refraction computations
 
-for nn=1:NN
-    for k=1:KK
-        %do the necessary Mie computations.
-        max_wl=wl(k)+3*delta_wl(k); %99th percentile.
-        min_wl=wl(k)-3*delta_wl(k); %99th percentile.
-        max_D=D0(nn)+1.5*err_D0(nn)+3*delta_D0(nn); %99th percentile.
-        min_D=D0(nn)-1.5*err_D0(nn)-3*delta_D0(nn); %99th percentile.
+	NN = length(D0); %number of beads
+	KK = length(wl); %number of wavelengths of beta 
+	JJ = length(c_wl); %number of wavelengths of beam_c
+	NNN = 500; %number of size parameter discritization wanted
+	N = 20000; % number of random realizations
+	now_1 = now; % start timer
 
-        %parameter space for which we will do Mie computations.
-        min_D_over_lambda=min_D/max_wl;
-        max_D_over_lambda=max_D/min_wl;
-        NNN=500; %number of size parameter discritization wanted
-        dd=(max_D_over_lambda-min_D_over_lambda)/NNN;
+	% initialise arrays used to collect output of for loops (this is key to speed up for loops in octave)
+	rho = nan(NNN + 1, 1);
+	beta__ = nan(NNN + 1, length(ang));
+	rr = nan(N, 1);
+	VSF = nan(NN, KK, length(ang));
+	d_VSF = nan(NN, KK, length(ang));
+	Q_ext = nan(NNN + 1, 1);
+	r = nan(N, 1);
+	c = nan(N, NN);
+	
+	beam_c = nan(NN, JJ);
+	d_beam_c = nan(NN, JJ);
+	
+    beta_ = nan(N, 1);
+	mean_beta_ = nan(KK, JJ);
+    d_beta_ = nan(KK, JJ);
+    ratio = nan(NN, KK, JJ);
+    unc_ratio = nan(NN, KK, JJ);
 
-        for i=1:NNN+1
-            [n, nm]=IoR(wl(k),T);
-            rho(i)=pi*real(n)*(min_D_over_lambda+(i-1)*dd); %rho
-            [S1 S2 Qb Qc Qback]=fastmie(rho(i),n,nang);
-            S11=0.5*((abs(S1)).^2 + (abs(S2)).^2);
-            S11_rad=S11.*sin(ang'/180*pi)*2*pi;
-            S11_int=integrate(S11_rad,dang,length(S11_rad));
-            beta__(i,:)=Qb*S11/S11_int; %table VSF
-        end
-        %sample randomly from wavelength and bead size space based on bead and wavelength distribution.
-        for j=1:20000
-            DD=normrnd(normrnd(D0(nn),err_D0(nn)/2),delta_D0(nn)); 
-            while DD<0
-                DD=normrnd(normrnd(D0(nn),err_D0(nn)/2),delta_D0(nn)); 
-            end
-            lambda=normrnd(wl(k),delta_wl(k));
-            rr(j)=pi*nm*DD/lambda;
-            beta(j,:)=pi*DD^2/4*interp1(rho,beta__,rr(j),'linear','extrap');
-        end
-        VSF(nn,k,:)=mean(beta);
-        d_VSF(nn,k,:)=std(beta);
-    end
+
+	for nn = 1: NN
+	    for k = 1: KK
+	        %do the necessary Mie computations.
+	        max_wl = wl(k) + 3*delta_wl(k); %99th percentile.
+	        min_wl = wl(k) - 3*delta_wl(k); %99th percentile.
+	        max_D = D0(nn) + err_D0(nn) + 3*delta_D0(nn); %99th percentile.
+	        min_D = D0(nn) - err_D0(nn) - 3*delta_D0(nn); %99th percentile.
+
+	        %parameter space for which we will do Mie computations.
+	        min_D_over_lambda = min_D/max_wl;
+	        max_D_over_lambda = max_D/min_wl;
+	        dd = (max_D_over_lambda - min_D_over_lambda)/NNN;
+
+	        for i = 1 : NNN + 1
+	            [n,  nm] = IoR(wl(k), T);
+	            rho(i) = pi*real(n)*(min_D_over_lambda + (i - 1)*dd); %rho
+	            [S1 S2 Qb Qc Qback] = fastmie(rho(i), n, nang);
+	            S11 = 0.5*((abs(S1)).^2  +  (abs(S2)).^2);
+	            S11_rad = S11.*sin(ang'/180*pi)*2*pi;
+	            S11_int = integrate(S11_rad, dang, length(S11_rad));
+	            beta__(i, :) = Qb*S11/S11_int; %table VSF
+	        end
+			
+	        %sample randomly from wavelength and bead size space based on bead and wavelength distribution.
+	        for j = 1: N
+	            DD = normrnd(normrnd(D0(nn), err_D0(nn)), delta_D0(nn));
+				if any(DD<=0)
+					keyboard()
+				end	
+	            lambda = normrnd(wl(k), delta_wl(k));
+				if any(lambda<=0)
+					keyboard()
+				end	
+	            rr(j) = pi*nm*DD/lambda;
+	            beta(j, :) = pi*DD^2/4*interp1(rho, beta__, rr(j), 'linear', 'extrap');
+	        end
+			
+	        VSF(nn, k, :) = nanmean(beta);
+	        d_VSF(nn, k, :) = nanstd(beta);
+	    end
+	end
+	clear rr
+
+
+	 %number of wavelengths of beta
+	for nn = 1: NN
+	    for jj = 1: JJ
+	        %do the necessary Mie computations.
+	        max_wl = c_wl(jj) + 3*delta_c_wl(jj); %99th percentile.
+	        min_wl = c_wl(jj) - 3*delta_c_wl(jj); %99th percentile.
+	        max_D = D0(nn) + err_D0(nn) + 3*delta_D0(nn); %99th percentile.
+	        min_D = D0(nn) - err_D0(nn) - 3*delta_D0(nn); %99th percentile.
+	        dd = (max_D_over_lambda - min_D_over_lambda)/900;
+
+	        for i = 1: NNN + 1
+	            [n,  nm] = IoR(c_wl(jj), T);
+	            rho(i) = pi*real(n)*(min_D_over_lambda + (i - 1)*dd); %rho
+	            [S1 S2 Qb Qc Qback] = fastmie(rho(i), n, nang);
+	            S11 = 0.5*((abs(S1)).^2  +  (abs(S2)).^2);
+	            S11_rad = S11.*sin(ang'/180*pi)*2*pi;
+	            S11_int = integrate(S11_rad, dang, length(S11_rad));
+	            %computations for beam attenuation: correcting for acceptance angle of ac - 9
+	            delta = (pi - accept_angle*pi/180)/length(ang);
+	            ang_ = [accept_angle*pi/180: delta: pi]*180/pi;
+	            nan_ = length(ang_);
+	            S11_ = interp1(ang, S11, ang_, 'pchip')';
+	            S11_rad_ = S11_.*sin(ang_'*pi/180)*2*pi; %taking care of azymuthal weighing
+	            S11_int_ = integrate(S11_rad_, delta, nan_);
+	            Qb_corr = Qb*S11_int_/S11_int; % correct Qb for acceptance angle.
+	            Qa = Qc - Qb;
+	            Q_ext(i) = Qa + Qb_corr;
+	        end
+		
+	        %sample randomly from wavelength and bead size space.
+	        for j = 1: N
+	            DD = normrnd(normrnd(D0(nn), err_D0(nn)), delta_D0(nn)); 
+				if any(DD<=0)
+					keyboard()
+				end	
+	            lambda = normrnd(c_wl(jj), delta_c_wl(jj));
+				if any(lambda<=0)
+					keyboard()
+				end	
+	            r(j) = pi*nm*DD/lambda;
+	            c(j, : ) = pi*DD^2/4*interp1(rho, Q_ext, r(j), 'linear', 'extrap');
+	        end
+			
+	        beam_c(nn, jj) = mean(c);
+	        d_beam_c(nn, jj) = std(c);
+	    end
+	end
+
+
+	for nn = 1: NN
+	    for k = 1: KK
+	        for jj = 1: JJ
+	          for j = 1: N %this is where the angular distribution of theta is taken into account
+	            theta_ = normrnd(normrnd(theta, err_theta), d_theta);
+				if any(theta_<=0)
+					keyboard()
+				end	
+	            if theta_>180
+	                theta_ = theta_ - 180;
+	            end
+	            beta_(j) = interp1(ang, normrnd(squeeze(VSF(nn, k, : )), squeeze(d_VSF(nn, k, : ))), theta_, 'linear');
+	          end
+			  
+	          mean_beta_(k, jj) = nanmean(beta_);
+	          d_beta_(k, jj) = nanstd(beta_);
+	          ratio(nn, k, jj) = mean_beta_(k, jj)./beam_c(nn, jj);
+	          unc_ratio(nn, k, jj) = sqrt((d_beta_(k, jj)/mean_beta_(k, jj)).^2 + (d_beam_c(nn, jj)/beam_c(nn, jj))^2)*ratio(nn, k, jj);
+	        end
+	    end
+	end
+
+	now_2 = now;
+	time_lapse = (now_2 - now_1)*24*3600 %in seconds
+	
+	return
 end
-clear rr
- %number of wavelengths of beta
-for nn=1:NN
-    for jj=1:JJ
-        %do the necessary Mie computations.
-        max_wl=c_wl(jj)+3*delta_c_wl(jj); %99th percentile.
-        min_wl=c_wl(jj)-3*delta_c_wl(jj); %99th percentile.
-        max_D=D0(nn)+1.5*err_D0(nn)+3*delta_D0(nn); %99th percentile.
-        min_D=D0(nn)-1.5*err_D0(nn)-3*delta_D0(nn); %99th percentile.
-        dd=(max_D_over_lambda-min_D_over_lambda)/900;
 
-        for i=1:NNN+1
-            [n, nm]=IoR(c_wl(jj),T);
-            rho(i)=pi*real(n)*(min_D_over_lambda+(i-1)*dd); %rho
-            [S1 S2 Qb Qc Qback]=fastmie(rho(i),n,nang);
-            S11=0.5*((abs(S1)).^2 + (abs(S2)).^2);
-            S11_rad=S11.*sin(ang'/180*pi)*2*pi;
-            S11_int=integrate(S11_rad,dang,length(S11_rad));
-            %computations for beam attenuation:correcting for acceptance angle of ac-9
-            delta=(pi-accept_angle*pi/180)/length(ang);
-            ang_=[accept_angle*pi/180:delta:pi]*180/pi;
-            nang_=length(ang_);
-            S11_=interp1(ang,S11,ang_,'pchip')';
-            S11_rad_=S11_.*sin(ang_'*pi/180)*2*pi; %taking care of azymuthal weighing
-            S11_int_=integrate(S11_rad_,delta,nang_);
-            Qb_corr=Qb*S11_int_/S11_int; % correct Qb for acceptance angle.
-            Qa=Qc-Qb;
-            Q_ext(i)=Qa+Qb_corr;
-        end
-        %sample randomly from wavelength and bead size space.
-        for j=1:20000
-            DD=normrnd(normrnd(D0(nn),err_D0(nn)/2),delta_D0(nn)); 
-            while DD<0
-                DD=normrnd(normrnd(D0(nn),err_D0(nn)/2),delta_D0(nn)); 
-            end
-            lambda=normrnd(c_wl(jj),delta_c_wl(jj));
-            r(j)=pi*nm*DD/lambda;
-            c(j,:)=pi*DD^2/4*interp1(rho,Q_ext,r(j),'linear','extrap');
-        end
-         beam_c(nn,jj)=mean(c);
-         d_beam_c(nn,jj)=std(c);
-    end
-end
-for nn=1:NN
-    for k=1:KK
-        for jj=1:JJ
-          for j=1:20000 %this is where the angular distribution of theta is taken into account
-            theta_=normrnd(normrnd(theta,err_theta),d_theta);
-            while theta_<0
-                theta_=normrnd(normrnd(theta,err_theta),d_theta);
-            end
-            if theta_>180
-                theta_=theta_-180;
-            end
-            beta_(j)=interp1(ang,normrnd(squeeze(VSF(nn,k,:)),squeeze(d_VSF(nn,k,:))),theta_,'linear');
-          end
-          mean_beta_(k,jj)=nanmean(beta_);
-          d_beta_(k,jj)=nanstd(beta_);
-          ratio(nn,k,jj)=mean_beta_(k,jj)./beam_c(nn,jj)
-          unc_ratio(nn,k,jj)=sqrt((d_beta_(k,jj)/mean_beta_(k,jj)).^2+(d_beam_c(nn,jj)/beam_c(nn,jj))^2)*ratio(nn,k,jj);
-        end
-    end
-end
 
-now_2=now;
-time_lapse=(now_2-now_1)*24*3600 %in seconds
-return
-end
 
-function [n, nm]=IoR(wl,T)
+function [n, nm] = IoR(wl, T)
 %function to compute the index of refraction of beads relative to pure water of
 %temperature T.
-np=1.5718 + 0.008412/(wl^2) + 0.000235/(wl^4); %Jones et al.
-ni=0.0003;  %imaginary part of index of refraction
-n0=1.31405; n1=1.779e-4; n2=-1.05e-6; n3=1.6e-8; n4=-2.02e-6; n5=15.868; n6=0.01155; n7=-0.00423; n8=-4382; n9=1.1455e6; %index of refraction of water Quan and Fry, 1995
-nm=n0+n4*T^2+(n5+n7*T)/(wl*1000)+n8/(wl*1000)^2+n9/(wl*1000)^3; %checked
-n=(np+ni*sqrt(-1))/nm; %index of refraction relative to water
-return
+	np = 1.5718  +  0.008412/(wl^2)  +  0.000235/(wl^4); %Jones et al.
+	ni = 0.0003;  %imaginary part of index of refraction
+	n0 = 1.31405; n1 = 1.779e-4; n2 = -1.05e-6; n3 = 1.6e-8; n4 = -2.02e-6; n5 = 15.868; n6 = 0.01155; n7 = -0.00423; n8 = -4382; n9 = 1.1455e6; %index of refraction of water Quan and Fry,  1995
+	nm = n0 + n4*T^2 + (n5 + n7*T)/(wl*1000) + n8/(wl*1000)^2 + n9/(wl*1000)^3; %checked
+	n = (np + ni*sqrt(-1))/nm; %index of refraction relative to water
+	return
 end
 
-function intgr=integrate(y,dx,N)
-%function to perform an integral of y with respect to x, with dx the
+
+
+function intgr = integrate(y, dx, N)
+%function to perform an integral of y with respect to x,  with dx the
 %increment and N the total number of y's to integrate.
-intgr=3/8*(y(1)+y(N))+7/6*(y(2)+y(N-1))+23/24*(y(3)+y(N-2));
-intgr=(intgr+sum(y(4:N-3)))*dx;
+	intgr = 3/8*(y(1) + y(N)) + 7/6*(y(2) + y(N - 1)) + 23/24*(y(3) + y(N - 2));
+	intgr = (intgr + sum(y(4: N - 3)))*dx;
 end
